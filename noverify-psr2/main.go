0709187affbd86a1a749a71d87d0eeeccda66645
Ilya Bogdanovich <bogdanovichiy@gmail.com>
2019-04-02 17:11:49 +0300

    use RootChecker as more appropriate in this case

--- noverify-psr2/main.go
+++ noverify-psr2/main.go
@@ -14,52 +14,37 @@ import (
 
 func main() {
 	log.SetFlags(log.Flags() | log.Lmicroseconds)
-	linter.RegisterBlockChecker(func(ctx *linter.BlockContext) linter.BlockChecker { return &block{ctx: ctx} })
+	linter.RegisterRootChecker(func(ctx *linter.RootContext) linter.RootChecker { return &block{ctx: ctx} })
 	cmd.Main()
 }
 
 type block struct {
-	linter.BlockCheckerDefaults
-	ctx *linter.BlockContext
+	linter.RootCheckerDefaults
+	ctx *linter.RootContext
 }
 
 func (b *block) BeforeEnterNode(w walker.Walkable) {
 	switch w.(type) {
-	case *stmt.Class:
-		cls := w.(*stmt.Class)
-		b.handleClassStatements(cls.Stmts)
+	case *stmt.ClassMethod:
+		b.checkModifiersOrder(w.(*stmt.ClassMethod).Modifiers)
+
+	case *stmt.PropertyList:
+		b.checkModifiersOrder(w.(*stmt.PropertyList).Modifiers)
 	}
 }
 
-func (b *block) handleClassStatements(stmts []node.Node) {
-	for _, s := range stmts {
-		switch s.(type) {
-		case *stmt.ClassMethod:
-			method := s.(*stmt.ClassMethod)
-			b.checkModifiersOrder(method, method.Modifiers)
-
-		case *stmt.PropertyList:
-			plist := s.(*stmt.PropertyList)
-			b.checkModifiersOrder(plist, plist.Modifiers)
-		}
-	}
-}
-
-func (b* block) checkModifiersOrder(method node.Node, modifiers []node.Node) {
-	visibilityModifiers := map[string]bool { "private": false, "protected": false, "public" : false }
-
-	var mStatic node.Node = nil
+func (b* block) checkModifiersOrder(modifiers []node.Node) {
+	var nStatic node.Node = nil
 
 	for _, m := range modifiers {
-		mValue := m.(*node.Identifier).Value
-		_, match := visibilityModifiers[mValue]
-		if (match) {
-			if mStatic != nil {
-				b.ctx.Report(mStatic, linter.LevelWarning, "PSR-2", "static MUST be declared after the visibility.")
+		switch v := m.(*node.Identifier).Value; v {
+		case "static":
+			nStatic = m
+		case "public", "private", "protected":
+			if nStatic != nil {
+				b.ctx.Report(nStatic, linter.LevelWarning, "PSR-2", "static MUST be declared after the visibility.")
 				return
 			}
-		} else if mValue == "static" {
-			mStatic = m
 		}
 	}
 }
